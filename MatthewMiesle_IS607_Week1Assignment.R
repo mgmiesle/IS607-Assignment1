65a35633de40ffb7f258bb27dcb71abe10eb93f3
mgmiesle <mgmiesle@yahoo.com>
2014-09-01 14:15:21 -0400

    First version of Assignment 1 submission

---
+++ MatthewMiesle_IS607_Week1Assignment.R
@@ -0,0 +1,23 @@
+# Matthew Miesle
+# CUNY SPS - IS 607
+# Week 1 Assignment
+# Due: 2014-09-02
+#
+# 1. What versions of R and RStudio do you have installed?
+#
+# R Version: 3.1.1
+# R Studio Version: 0.98.1028
+# 
+# 2. What version of PostgreSQL do you have installed?
+#
+# PostgreSQL Version: 9.3.5
+# 
+# 3. Install and load the R package DMwR. Load the data set sales and determine
+# the number of observations contained in the data set.
+# (In RStudio, this is easy to determine.)
+#
+# The number of observations in the sales data set is 401,146.
+# 
+#  
+# Submit your answers to these questions by placing your script in a
+# public repository on github and submitting a link to the script here.
